60798c6ee1f2d56f6bbef4f98b9db2e32576029c
dpfla <dpfla73@gmail.com>
2022-10-16 17:49:13 +0900

    todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [modifyMode, setModifyMode] = useState([]);
+
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(todo === ""){
+      return ;
+    }
+    setTodos((curArray) => [todo, ...curArray]);
+    setModifyMode((curArray) => [false, ...curArray]);
+    setTodo("");
+  };
+  const onDelete = (index) => {
+    setTodos((curArray) => 
+      curArray.filter((_, curi) => curi!==index));
+  };
+  const onModifyMode = (index) => {
+    setModifyMode((curArray) => 
+      curArray.map((cur, curi) => {
+        if(curi === index)
+          return !cur;
+        return cur;
+      })
+    )
+  };
+  const onModify = (event, index) => {
+    setTodos((curArray) => 
+      curArray.map((cur, curi) => {
+        if(curi === index)
+          return event.target.value;
+        return cur;
+      })
+    )
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <h1>My Todo List</h1>
+      <form onSubmit={onSubmit} className="inputForm">
+        <input 
+          onChange={onChange} 
+          value={todo} 
+          type="text" 
+          placeholder="Write your to do..." 
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((item, index) => (
+            <li key={index}>
+              <span>
+                {modifyMode[index] ? <input value={item} onChange={(event) => onModify(event, index)}></input> : <span>{item}</span>}
+              </span>
+              <span>
+                <button onClick={() => onModifyMode(index)}>수정</button>
+                <button onClick={() => onDelete(index)}>삭제</button>
+              </span>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
